12266471922b31f9781ad382662276fc8fcc2836
koo04 <douglas@devicariis.org>
2025-05-09 07:14:37 -0400

    Progress?

--- neovim/switch_nvim.sh
+++ neovim/switch_nvim.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# get the folders in ../neovim/.config
-dir=($(find .config/ -maxdepth 1 -type d -not -name '.config' -not -name '..' | sort))
+if ! command -v fzf &>/dev/null; then
+  sudo apt install fzf
+fi
 
-config=$(printf "%s\n" "${dir[@]}" | fzf --prompt="Neovim Config " --height=~50% --layout=reverse --border --exit-0)
+dir=($(find $(dirname "$0")/.config/ -maxdepth 1 -type d -not -name '.config' -not -name '..' | sort))
+config=$(printf "%s\n" "${dir[@]}" | fzf --prompt="Neovim Config " --layout=reverse --border --exit-0)
 if [[ -z $config ]]; then
-    echo "Nothing selected"
-    exit 1
+  echo "Nothing selected"
+  exit 1
 elif [[ $config == "v0" ]]; then
-    config=""
+  config=""
 fi
 config=${config#*.config/}
 config=${config%/}
 config=${config#./}
 
-NVIM_APPNAME=$config nvim $@
+# NVIM_APPNAME=$config nvim $@
